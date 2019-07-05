025af89a48416d9bdbff21f5c8a76d95ac53eceb
Maxim <maxden33@gmail.com>
2019-07-05 12:49:58 +0400

    Update RefUserApiController.java
    Добавил работу с базой данных на примере RefUser.

--- src/main/java/io/swagger/api/RefUserApiController.java
+++ src/main/java/io/swagger/api/RefUserApiController.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +22,7 @@ import javax.validation.constraints.*;
 import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-07-04T12:34:26.681Z[GMT]")
@@ -31,6 +35,9 @@ public class RefUserApiController implements RefUserApi {
 
     private final HttpServletRequest request;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @org.springframework.beans.factory.annotation.Autowired
     public RefUserApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
@@ -39,7 +46,17 @@ public class RefUserApiController implements RefUserApi {
 
     public ResponseEntity<List<RefUser>> refUserGet(@ApiParam(value = "id пользователя.") @Valid @RequestParam(value = "id", required = false) Integer id) {
         String accept = request.getHeader("Accept");
-        return new ResponseEntity<List<RefUser>>(HttpStatus.NOT_IMPLEMENTED);
+
+        List<RefUser> users = new ArrayList<>();
+
+        String sql = "SELECT id, first_name, middle_name, last_name, username, password, email FROM ref_user WHERE id = ?";
+        RefUser refUser = (RefUser) jdbcTemplate.queryForObject(
+                sql, new Object[]{id},
+                new BeanPropertyRowMapper(RefUser.class));
+        users.add(refUser);
+
+        return new ResponseEntity<List<RefUser>>(users, HttpStatus.OK);
+
     }
 
 }
