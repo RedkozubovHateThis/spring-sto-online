a852666adedffaca0a583884fbed05056c186534
maxden33@gmail.com <maxden33@gmail.com>
2019-08-04 00:56:42 +0300

    Получение нарядов и фаербейз.

--- src/main/java/io/swagger/Application.java
+++ src/main/java/io/swagger/Application.java
@@ -1,7 +1,21 @@
 package io.swagger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.print.Doc;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Application {
--- src/main/resources/migration/postgresql/V15.1__restart_user_sequence.sql
+++
@@ -1,1 +0,0 @@
-ALTER SEQUENCE user__id_seq RESTART WITH 5;
