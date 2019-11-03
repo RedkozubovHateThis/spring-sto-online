df469d5ad007607155ce9541437759ea9a19122a
Евгений Мокин <stingrayss2@gmail.com>
2019-11-03 15:24:29 +0500

    Переделал пагинацию для мобильных устройств

--- webSto-UI/src/app/pages/pagination.ts
+++ webSto-UI/src/app/pages/pagination.ts
@@ -36,12 +36,61 @@ export abstract class Pagination implements OnInit {
 
   setPageData(data: Pageable<any>) {
     this.totalPages = data.totalPages;
+    const pageInfo = this.page + 1;
 
     if ( this.totalPages != null ) {
 
       this.pagesInfo = [];
-      for ( let p:number = 1; p <= this.totalPages; p++ ) {
-        this.pagesInfo.push(p);
+      // for ( let p:number = 1; p <= this.totalPages; p++ ) {
+      //   this.pagesInfo.push(p);
+      // }
+
+      if (this.totalPages <= 6) {
+        for (let p: number = 1; p <= this.totalPages; p++) {
+          this.pagesInfo.push(p);
+        }
+      }
+      else {
+        // Always print first page button
+        this.pagesInfo.push(1);
+
+        // Print "..." only if currentPage is > 3
+        // if (pageInfo > 3) {
+        //   this.pagesInfo.push(null);
+        // }
+
+        // special case where last page is selected...
+        if (pageInfo === this.totalPages) {
+          this.pagesInfo.push(pageInfo - 2);
+        }
+
+        // Print previous number button if currentPage > 2
+        if (pageInfo > 2) {
+          this.pagesInfo.push(pageInfo - 1);
+        }
+
+        // Print current page number button as long as it not the first or last page
+        if (pageInfo !== 1 && pageInfo !== this.totalPages) {
+          this.pagesInfo.push(pageInfo);
+        }
+
+        // print next number button if currentPage < lastPage - 1
+        if (pageInfo < this.totalPages - 1) {
+          this.pagesInfo.push(pageInfo + 1);
+        }
+
+        // special case where first page is selected...
+        if (pageInfo === 1) {
+          this.pagesInfo.push(pageInfo + 2);
+        }
+
+        // print "..." if currentPage is < lastPage -2
+        // if (pageInfo < this.totalPages - 2) {
+        //   this.pagesInfo.push(null);
+        // }
+
+        // Always print last page button if there is more than 1 page
+        this.pagesInfo.push(this.totalPages);
       }
 
     }
