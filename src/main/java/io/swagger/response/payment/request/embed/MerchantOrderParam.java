1d70e2093a0ed7d3e6159dc0f462e4276df77bd2
Евгений Мокин <stingrayss2@gmail.com>
2020-01-09 22:12:40 +0500

    Release 1.0-RELEASE & 1.1.1

--- src/main/java/io/swagger/response/payment/request/embed/MerchantOrderParam.java
+++ src/main/java/io/swagger/response/payment/request/embed/MerchantOrderParam.java
@@ -1,7 +1,9 @@
 package io.swagger.response.payment.request.embed;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class MerchantOrderParam extends Attribute {
 }
